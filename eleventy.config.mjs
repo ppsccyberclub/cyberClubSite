1a8965cb70f9b6e89226e79db94a2ca8a79eb3c9
Charity Gamble <cgamble.dev@proton.me>
2026-08-24 22:02:33 -0600

    "login page" completed for now

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -1,9 +1,8 @@
 export default async function(eleventyConfig) {
-    //Configure Build Awesome
     eleventyConfig.setInputDirectory('src');
     eleventyConfig.setOutputDirectory('public');
-    eleventyConfig.setIncludesDirectory('src/_includes');
-    eleventyConfig.addPassthroughcopy('src/assets');
+    eleventyConfig.setIncludesDirectory('_includes');
+    eleventyConfig.addPassthroughCopy('src/assets');
     eleventyConfig.addPreprocessor("drafts", "*", (data, content) => {
         if(data.draft && process.env.ELEVENTY_RUN_MODE === "build") {
             return false;
